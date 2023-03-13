7bbe26a717c520478d362e86fb5d072126479202
Loic Nicolle <loic.nicolle@orange.com>
2023-03-13 10:31:03 +0100

    Add a testcase to ensure the dump file is available

--- tools/gci-templates/scripts/units-reports.py
+++ tools/gci-templates/scripts/units-reports.py
@@ -30,28 +30,39 @@ env_type = args.env_type
 timestampFormat = '%Y-%m-%dT%H:%M:%SZ'
 test_cases = []
 
+short_filename = os.path.basename(os.path.split(input_filename)[0]) + "/" + os.path.basename(input_filename)
+
 try:
     f = open(input_filename)
     data = yaml.load(f, Loader=SafeLoader)
-except IOError:
-    print(input_filename + " was not found, units-report.xml will not be generated")
-    exit(1)
-
-for unit in data['items']:
-
-    unit_name = unit['metadata']['name']
-    unit_creation_timestamp = unit['metadata']['creationTimestamp']
-    unit_ready_status = [status for status in unit['status']['conditions'] if status['type'] == 'Ready']
-
-    tstamp1 = datetime.strptime(unit_creation_timestamp, timestampFormat)
-    tstamp2 = datetime.strptime(unit_ready_status[0]['lastTransitionTime'], timestampFormat)
-    td = tstamp2 - tstamp1
-
-    print("Generating testcase for " + unit_name)
-    test_case = TestCase(env_type + ":" + unit_name, classname='sylva-unit-validation', elapsed_sec=td.total_seconds())
-    if unit_ready_status[0]['status'] != 'True':
-        test_case.add_failure_info(unit_name + " unit ready status was: " + unit_ready_status[0]['status'])
+    test_case = TestCase(env_type + ":dump-check:" + short_filename, classname='sylva-unit-validation')
     test_cases.append(test_case)
+
+    for unit in data['items']:
+
+        unit_name = unit['metadata']['name']
+        unit_creation_timestamp = unit['metadata']['creationTimestamp']
+        unit_ready_status = [status for status in unit['status']['conditions'] if status['type'] == 'Ready']
+
+        tstamp1 = datetime.strptime(unit_creation_timestamp, timestampFormat)
+        tstamp2 = datetime.strptime(unit_ready_status[0]['lastTransitionTime'], timestampFormat)
+        td = tstamp2 - tstamp1
+
+        print("Generating testcase for " + unit_name)
+        test_case = TestCase(env_type + ":" + unit_name, classname='sylva-unit-validation',
+                             elapsed_sec=td.total_seconds())
+        if unit_ready_status[0]['status'] != 'True':
+            test_case.add_failure_info(unit_name + " unit ready status was: " + unit_ready_status[0]['status'])
+        test_cases.append(test_case)
+
+except IOError:
+    print(input_filename + " was not found")
+    test_case = TestCase(env_type + ":dump-check:" + short_filename, classname='sylva-unit-validation')
+    test_case.add_failure_info(short_filename + " was not found: ")
+    test_cases.append(test_case)
+    ts = TestSuite(env_type + ":" + "sylva-unit-validation", test_cases)
+
+
 
 ts = TestSuite(env_type + ":" + "sylva-unit-validation", test_cases)
 
