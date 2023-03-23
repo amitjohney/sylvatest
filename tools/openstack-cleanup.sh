b7b189d9af232308b2f80902087d3f2609e9ef6c
Bogdan-Adrian Burciu <bogdanadrian.burciu@yahoo.com>
2023-03-23 08:10:22 +0000

    Small debug additions to openstack-cleanup.sh

--- tools/openstack-cleanup.sh
+++ tools/openstack-cleanup.sh
@@ -2,8 +2,6 @@
 
 # Helper script used to clean management and test workload clusters OpenStack resources. USE WITH CARE, AT YOU OWN RISK
 
-set -o xtrace
-
 OS_ARGS=""
 PLATFORM=$1
 if [ ! -z $PLATFORM ]; then
@@ -17,22 +15,24 @@ if openstack ${OS_ARGS} endpoint list &> /dev/null; then
     exit 1
 fi
 
-openstack ${OS_ARGS} server list --tags ${CAPO_TAG} -f value -c Name | awk '{print $1}' | xargs -r openstack ${OS_ARGS} server delete --wait
+openstack ${OS_ARGS} server list --tags ${CAPO_TAG} -f value -c Name | awk '{print $1}' | xargs -tr openstack ${OS_ARGS} server delete --wait
 
-openstack ${OS_ARGS} port list --tags ${CAPO_TAG} -f value -c name -c status -c device_owner -c id | awk '$2=="DOWN" {print $4}' | xargs -r openstack ${OS_ARGS} port delete || true
+openstack ${OS_ARGS} port list --tags ${CAPO_TAG} -f value -c name -c status -c device_owner -c id | awk '$2=="DOWN" {print $4}' | xargs -tr openstack ${OS_ARGS} port delete || true
 
-openstack ${OS_ARGS} security group list --tags ${CAPO_TAG} -f value -c ID | xargs -r openstack ${OS_ARGS} security group delete || true
+openstack ${OS_ARGS} security group list --tags ${CAPO_TAG} -f value -c ID | xargs -tr openstack ${OS_ARGS} security group delete || true
 
-openstack ${OS_ARGS} stack list --tags ${CAPO_TAG} -f value -c ID | xargs -r openstack ${OS_ARGS} stack delete || true
+openstack ${OS_ARGS} stack list --tags ${CAPO_TAG} -f value -c ID | xargs -tr openstack ${OS_ARGS} stack delete || true
 
 for vol in $(openstack ${OS_ARGS} volume list --status available -f value -c Name | grep ^pvc); do
     vol_property=$(openstack ${OS_ARGS} volume show $vol -c properties -f json | jq '.properties."cinder.csi.openstack.org/cluster"' -r)
     if [ "${vol_property}" = "${CAPO_TAG}" ]; then
+        echo "openstack ${OS_ARGS} volume delete $vol --purge"
         openstack ${OS_ARGS} volume delete $vol --purge
     fi
 done
 
-if [[ $(openstack ${OS_ARGS} server list --tags ${CAPO_TAG}) ]]; then
-    echo "There CAPO machines tagged ${CAPO_TAG} were not removed, please try again"
+if [ -n "$(openstack ${OS_ARGS} server list -f value -c ID)" ]; then
+    echo "The following CAPO machines tagged ${CAPO_TAG} were not removed, please try again."
+    openstack ${OS_ARGS} server list --tags ${CAPO_TAG} -f value -c Name
     exit 1
 fi
