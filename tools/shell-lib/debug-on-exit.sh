a617608d9a7a61f3ac19baf94d97c5efa6c92c43
Thomas Morin <thomas.morin@orange.com>
2023-04-19 11:48:32 +0200

    more debug-on-exit

--- tools/shell-lib/debug-on-exit.sh
+++ tools/shell-lib/debug-on-exit.sh
@@ -2,7 +2,7 @@
 
 function dump_flux_resources() {
     cluster_dir=$1
-    echo "Dumping Flux resources to $cluster_dir"
+    echo "Dumping Flux resources"
     for kind in gitrepositories helmcharts helmrepositories helmreleases kustomizations ; do
         kubectl get $kind -o wide > $cluster_dir/flux-$kind.summary.txt
         kubectl get $kind -o yaml > $cluster_dir/flux-$kind.yaml
@@ -24,6 +24,10 @@ dump_flux_resources bootstrap-cluster-dump
 if [[ -f $BASE_DIR/management-cluster-kubeconfig ]]; then
     export KUBECONFIG=${KUBECONFIG:-$BASE_DIR/management-cluster-kubeconfig}
 
+    echo "Gathering info on mgmt cluster..."
+    mkdir management-cluster-dump
+    pushd management-cluster-dump
+
     echo "Get nodes in management cluster"
     kubectl --request-timeout=3s get nodes
 
@@ -31,9 +35,26 @@ if [[ -f $BASE_DIR/management-cluster-kubeconfig ]]; then
     kubectl --request-timeout=3s get pods -A
 
     echo "Performing dump on management cluster"
-    kubectl cluster-info dump -A -o yaml --output-directory=management-cluster-dump
+    kubectl cluster-info dump -A -o yaml --output-directory=.
 
-    dump_flux_resources management-cluster-dump
+    kubectl get crds > crds.txt
+    kubectl get crds -o yaml > crds.yaml
+
+    dump_flux_resources .
+
+    echo "Getting sylva-units values.units"
+    kubectl get secrets sylva-units-values-debug -o yaml | yq .data.values | base64 -d | yq .units > sylva-units.values-units.yaml
+
+    for r in statefulsets jobs cronjobs cluster machine pods pv pvc; do
+        kubectl get $r -A -o wide >> $r.txt
+        kubectl get $r -A -o yaml >> $r.yaml
+    done
+
+    for helmrelease in postgres cis-operator; do
+        helm history $helmrelease >> helm-$helmrelease.txt
+        helm get values $helmrelease -a >> helm-$helmrelease.txt
+        helm get manifest $helmrelease >> helm-$helmrelease.txt
+    done
 fi
 
 echo "Dump node logs"
