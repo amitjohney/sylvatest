bedfd34ed102f8d7219edd9edafad833d86b7463
manik bindlish <manik.bindlish@orange.com>
2023-05-26 16:53:10 +0000

    Typo cleanups: Cosmetic fix for kustomization

--- kustomize-units/kube-job/kube-job.sh
+++ kustomize-units/kube-job/kube-job.sh
@@ -10,7 +10,7 @@ echo "This script should be overwritten, otherwise it won't do anything but prin
 #kubectl wait --for condition=ControlPlaneReady --timeout 600s --all cluster
 #kubectl wait --for condition=NodeHealthy --timeout 600s --all machine
 #
-#echo "Freeze reconciliation on Kustomisations in source cluster that related to the management cluster"
+#echo "Freeze reconciliation on Kustomizations in source cluster that related to the management cluster"
 #kubectl annotate kustomizations -l suspend-on-pivot=yes kustomize.toolkit.fluxcd.io/reconcile=disabled --overwrite
 #
 #echo "Move cluster definitions from source to target cluster"
