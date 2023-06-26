95bfe0d98c3e37b6e7cb3785478e3813ea7e5e98
Priya Goyal <priya.goyal@orange.com>
2023-06-26 18:14:24 +0530

    typos

--- tools/shell-lib/common.sh
+++ tools/shell-lib/common.sh
@@ -133,8 +133,7 @@ function force_reconcile() {
 }
 
 function define_source() {
-  sed "s/CURRENT_COMMIT/${CURRENT_COMMIT}/" "$@" | sed "s,SYLVA_CORE_REPO,${SYLVA_CORE_REPO},g"
-
+  sed "s/CURRENT_COMMIT/${CURRENT_COMMIT}/" "$@" | sed "s,SYLVA_CORE_REPO,${SYLVA_CORE_REPO},g" "$@"
 }
 
 function inject_bootstrap_values() {
