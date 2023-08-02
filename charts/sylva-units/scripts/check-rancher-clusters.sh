37e750860bb90c25ea4efc763f983bd719a30fc3
bogdan.antohe <bogdan.antohe@orange.com>
2023-08-02 15:03:09 +0000

    Reconfigure kube-jobs

--- charts/sylva-units/scripts/check-rancher-clusters.sh
+++ charts/sylva-units/scripts/check-rancher-clusters.sh
@@ -4,18 +4,6 @@
 set -e
 set -o pipefail
 
-echo "-- Wait for Rancher cluster resources to be ready and picked up by capi-rancher-import operator"
-attempts=0
-max_attempts=5
-until kubectl wait --for condition=Ready --timeout 120s kustomizations.kustomize.toolkit.fluxcd.io -n workload-cluster cattle-agent-${CLUSTER_NAME}; do
-sleep 3
-((attempts++)) && ((attempts==max_attempts)) && exit -1
-done
-until kubectl wait --for=jsonpath='{.status.ready}'=true clusters.provisioning.cattle.io --all -n workload-cluster ; do
-sleep 3
-((attempts++)) && ((attempts==max_attempts)) && exit -1
-done
-
 echo "-- Retrieve randomly generated Rancher bootstrap password"
 BOOTSTRAP_PASSWORD=`kubectl -n cattle-system get secret bootstrap-secret -o jsonpath='{.data.bootstrapPassword}' | base64 -d`
 
@@ -23,7 +11,7 @@ echo "-- Login Rancher server and retrieve login token good for 10 minute"
 USERNAME=admin
 LOGIN_TOKEN=`curl --insecure -s https://rancher.cattle-system.svc.cluster.local/v3-public/localProviders/local?action=login -H 'content-type: application/json' --data-binary '{"username":"'$USERNAME'","password":"'$BOOTSTRAP_PASSWORD'","ttl":600000}' | jq -r .token`
 
-if [ -z "${LOGIN_TOKEN-unset}" ] || [ "${LOGIN_TOKEN}" = "null" ] ; then
+if [ -z "${LOGIN_TOKEN}" ] || [ "${LOGIN_TOKEN}" = "null" ] ; then
     echo "LOGIN_TOKEN is set to the empty string, will try again"
     exit 1
 fi
@@ -34,9 +22,9 @@ curl --insecure -s https://rancher.cattle-system.svc.cluster.local/v3/clusters/ 
 echo "-- Get kubeconfig URL for the '${CLUSTER_NAME}'"
 KUBECONFIG_URL=`curl --insecure -s https://rancher.cattle-system.svc.cluster.local/v3/clusters/  -H "Authorization: Bearer $LOGIN_TOKEN" | jq '.data[] | select (.name=="'${CLUSTER_NAME}'-capi") | .actions.generateKubeconfig' | tr -d '"'`
 echo $KUBECONFIG_URL
-curl --insecure -s -X POST $KUBECONFIG_URL  -H "Authorization: Bearer $LOGIN_TOKEN" | jq ".config" > kubeconfig
+curl --insecure -s -X POST $KUBECONFIG_URL  -H "Authorization: Bearer $LOGIN_TOKEN" | jq ".config" > /tmp/kubeconfig
 
 echo "-- Create generic secret workload-cluster-rancher-kubeconfig with the generated kubeconfig"
-kubectl create secret generic workload-cluster-rancher-kubeconfig --from-file=kubeconfig
+kubectl -n cattle-system  create secret generic workload-cluster-rancher-kubeconfig --from-file=/tmp/kubeconfig
 
 echo "-- All done"
