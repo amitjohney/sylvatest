4b74dfb68ec7161f337e83d401ff461965060636
Cristina Postole <cristina.postole@orange.com>
2023-09-04 15:10:42 +0000

    Test to validate the deployment of a pod on a workload cluster

--- charts/sylva-units/scripts/check-rancher-clusters.sh
+++ charts/sylva-units/scripts/check-rancher-clusters.sh
@@ -19,9 +19,9 @@ done
 echo "-- Retrieve randomly generated Rancher bootstrap password"
 BOOTSTRAP_PASSWORD=`kubectl -n cattle-system get secret bootstrap-secret -o jsonpath='{.data.bootstrapPassword}' | base64 -d`
 
-echo "-- Login Rancher server and retrieve login token good for 1 minute"
+echo "-- Login Rancher server and retrieve login token good for 10 minute"
 USERNAME=admin
-LOGIN_TOKEN=`curl --insecure -s https://rancher.cattle-system.svc.cluster.local/v3-public/localProviders/local?action=login -H 'content-type: application/json' --data-binary '{"username":"'$USERNAME'","password":"'$BOOTSTRAP_PASSWORD'","ttl":60000}' | jq -r .token`
+LOGIN_TOKEN=`curl --insecure -s https://rancher.cattle-system.svc.cluster.local/v3-public/localProviders/local?action=login -H 'content-type: application/json' --data-binary '{"username":"'$USERNAME'","password":"'$BOOTSTRAP_PASSWORD'","ttl":600000}' | jq -r .token`
 
 if [ -z "${LOGIN_TOKEN-unset}" ] || [ "${LOGIN_TOKEN}" = "null" ] ; then
     echo "LOGIN_TOKEN is set to the empty string, will try again"
@@ -30,5 +30,13 @@ fi
 
 echo "-- Show the clusters known to Rancher"
 curl --insecure -s https://rancher.cattle-system.svc.cluster.local/v3/clusters/  -H "Authorization: Bearer $LOGIN_TOKEN" | jq '.data[]  | .name, .labels, .conditions[6], .conditions[-2]'
+
+echo "-- Get kubeconfig URL for the '${CLUSTER_NAME}'"
+KUBECONFIG_URL=`curl --insecure -s https://rancher.cattle-system.svc.cluster.local/v3/clusters/  -H "Authorization: Bearer $LOGIN_TOKEN" | jq '.data[] | select (.name=="'${CLUSTER_NAME}'-capi") | .actions.generateKubeconfig' | tr -d '"'`
+echo $KUBECONFIG_URL
+curl --insecure -s -X POST $KUBECONFIG_URL  -H "Authorization: Bearer $LOGIN_TOKEN" | jq ".config" > kubeconfig
+
+echo "-- Create generic secret workload-cluster-rancher-kubeconfig with the generated kubeconfig"
+kubectl create secret generic workload-cluster-rancher-kubeconfig --from-file=kubeconfig
 
 echo "-- All done"
