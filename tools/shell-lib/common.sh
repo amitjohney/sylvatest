143b4fe203f9cbd941361f74536979686437aca9
Cristina Postole <cristina.postole@orange.com>
2023-12-04 12:45:48 +0000

    Update file common.sh

--- tools/shell-lib/common.sh
+++ tools/shell-lib/common.sh
@@ -17,6 +17,12 @@ else
   export IN_CI=0
 fi
 
+if [[ $# -eq 1 && -f $1 ]]; then
+    VALUES_FILE=$1
+else
+    VALUES_FILE=${ENV_PATH}/values.yaml
+fi
+
 if ! [[ $# -eq 1 && (-f ${1}/kustomization.yaml || -L ${1}/kustomization.yaml) ]]; then
     echo "Usage: $0 [env_name]"
     echo "This script expects to find a kustomization in [env_name] directory to generate management-cluster configuration and secrets"
