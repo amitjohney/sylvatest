4a58db72de4be0672376105efccb2c39f5e49bba
Daniel Anton <cristian.manda@orange.com>
2023-12-13 13:04:23 +0000

    Add the user provided info to the image details

--- charts/sylva-units/scripts/create-os-images-info.sh
+++ charts/sylva-units/scripts/create-os-images-info.sh
@@ -8,17 +8,15 @@ yq '.osImages | keys | .[]' /opt/images.yaml | while read line; do
   uri=$(yq '.osImages.[env(LINE)].uri' /opt/images.yaml)
   if [[ "$uri" == *"sylva-elements/diskimage-builder"* ]]; then
     echo "This is a Sylva diskimage-builder image. Updating image details from artifact"
-    echo "        uri: $uri" >> /tmp/os-image-details.yaml
     url=$(echo $uri| sed 's|oci://||')
     # Get artifact annotations and insert them as image details
     manifest=$(oras manifest fetch $url)
     echo $manifest | yq '.annotations |with_entries(select(.key|contains("sylva")))' -P | sed "s|.*/|        |" >> /tmp/os-image-details.yaml
-  else
-    echo "Updating image details for custom image"
-    yq '.osImages.[env(LINE)]' /opt/images.yaml | sed 's/^/        /' >> /tmp/os-image-details.yaml
   fi
+  echo "Adding user provided details"
+  yq '.osImages.[env(LINE)]' /opt/images.yaml | sed 's/^/        /' >> /tmp/os-image-details.yaml
 done
-# Verify os-images-info configmap or create an empty one
+# Verify os-images-info configmap exists or create an empty one
 if ! kubectl -n sylva-system get cm os-images-info &>/dev/null; then
   # Initialize new configmap
   kubectl -n sylva-system create configmap os-images-info
