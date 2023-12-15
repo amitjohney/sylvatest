c83c1c3e2e574ee4516e5e66462a910fd4862b37
Alain Thioliere <alain.thioliere@orange.com>
2023-12-15 10:33:03 +0000

    improve debug messages of the cluster-creator script

--- kustomize-units/cluster-creator-login/cluster-creator-login.sh
+++ kustomize-units/cluster-creator-login/cluster-creator-login.sh
@@ -22,49 +22,69 @@ if [ $? -ne 0 ]; then
 fi
 
 # Login token for the admin user
-ADMINTOKEN=$(curl -k -s $RANCHER_PUBLIC_API/localProviders/local?action=login -H 'content-type: application/json' --data-binary '{"username":"admin","password":"'$ADMIN_PASSWORD'","ttl":10}' | jq -r .token)
-if [ $? -ne 0 ]; then
-  echo "Could not login to rancher with the admin account"
+REQ_TOKEN=$(curl -k -s $RANCHER_PUBLIC_API/localProviders/local?action=login -H 'content-type: application/json' --data-binary '{"username":"admin","password":"'$ADMIN_PASSWORD'","ttl":10}')
+ADMINTOKEN=$(echo $REQ_TOKEN | jq -r .token)
+if [ $ADMINTOKEN == "null" ]; then
+  echo "Problem when authenticating as the admin user"
+  echo $REQ_TOKEN
   exit 1
 fi
+
 echo "Obtained a token for the admin user"
 
 # Check if cluster-creator is already created
-USER_CREATED=$(curl -k -s -H "Authorization: Bearer $ADMINTOKEN" $RANCHER_API/users -H 'content-type: application/json' | jq -r '.data[]|select(.username=="'$USERNAME'")|.id' | wc -l)
+USERS=$(curl -k -s -H "Authorization: Bearer $ADMINTOKEN" $RANCHER_API/users -H 'content-type: application/json')
+if [ $? -ne 0 ]; then
+  echo "Couldn't list the users"
+  echo $USERS
+fi
+
+USER_CREATED=$(echo $USERS | jq -r '.data[]|select(.username=="'$USERNAME'")|.id' | wc -l)
 if [ $USER_CREATED -eq 0 ]; then
+  echo "Creating the cluster-creator user"
   # Create the cluster-creator user
-  USERID=$(curl -k -s -H "Authorization: Bearer $ADMINTOKEN" $RANCHER_API/users -H 'content-type: application/json' --data-binary '{"me":false,"mustChangePassword":false,"type":"user","username":"'$USERNAME'","password":"'$CLUSTER_CREATOR_PASSWORD'","name":"'$USERNAME'"}' | jq -r .id)
+  REQ_USERID=$(curl -k -s -H "Authorization: Bearer $ADMINTOKEN" $RANCHER_API/users -H 'content-type: application/json' --data-binary '{"me":false,"mustChangePassword":false,"type":"user","username":"'$USERNAME'","password":"'$CLUSTER_CREATOR_PASSWORD'","name":"'$USERNAME'"}')
+  USERID=$(echo $REQ_USERID | jq -r .id)
+  if [ $USERID == "null" ]; then
+    echo "Problem when creating the cluster-creator user"
+    echo $REQ_USERID
+    exit 1
+  fi
   echo "User $USERID created"
 
   # Assign role
-  curl -k -s -H "Authorization: Bearer $ADMINTOKEN" $RANCHER_API/globalrolebinding -H 'content-type: application/json' --data-binary '{"type":"globalRoleBinding","globalRoleId":"'$GLOBAL_ROLE'","userId":"'$USERID'"}' > /dev/null
-  if [ $? -ne 0 ]; then
+  REQ_ROLEBINDING=$(curl -k -s -H "Authorization: Bearer $ADMINTOKEN" $RANCHER_API/globalrolebinding -H 'content-type: application/json' --data-binary '{"type":"globalRoleBinding","globalRoleId":"'$GLOBAL_ROLE'","userId":"'$USERID'"}')
+  if [ $? -ne 0 ] || [ $(echo $REQ_ROLEBINDING | jq -r ".baseType") == "error" ]; then
     echo "Could not assign the $GLOBAL_ROLE role to the cluster-creator user with id $USERID"
     exit 1
   fi
   echo "Role $GLOBAL_ROLE assigned to the cluster-creator user"
 else
   # The user is already created, we pick its id
-  USERID=$(curl -k -s -H "Authorization: Bearer $ADMINTOKEN" $RANCHER_API/users -H 'content-type: application/json' | jq -r '.data[]|select(.username=="'$USERNAME'")|.id')
+  USERID=$(echo $USERS | jq -r '.data[]|select(.username=="'$USERNAME'")|.id')
   echo "The user $USERNAME already exists with userid $USERID"
 fi
 if [ "$USERID" = "" ]; then
   echo "Could not obtain the user id of the $USERNAME user"
+  echo "Existing users: $USERS"
   exit 1
 fi
 
 # Login token that never expires for the sake of being fully declarative
-CREATORTOKEN=$(curl -k -s $RANCHER_PUBLIC_API/localProviders/local?action=login -H 'content-type: application/json' --data-binary '{"username":"'$USERNAME'","password":"'$CLUSTER_CREATOR_PASSWORD'","ttl":0}' | jq -r .token)
-if [ $? -ne 0 ]; then
+REQ_CREATORTOKEN=$(curl -k -s $RANCHER_PUBLIC_API/localProviders/local?action=login -H 'content-type: application/json' --data-binary '{"username":"'$USERNAME'","password":"'$CLUSTER_CREATOR_PASSWORD'","ttl":0}')
+CREATORTOKEN=$(echo $REQ_CREATORTOKEN | jq -r .token)
+if [ $CREATORTOKEN == "null" ]; then
   echo "Could not login to rancher with the cluster-creator account"
   exit 1
 fi
-echo "Obtained a token for the cluster-creator user"
+echo "Obtained a token for the cluster-creator user" $CREATORTOKEN
 
 # Get the kubeconfig for the cluster-creator user in the management cluster
-KUBECONFIG=$(curl -k -s -X POST -H "Authorization: Bearer $CREATORTOKEN" $RANCHER_API/clusters/local?action=generateKubeconfig | jq -r '.config')
-if [ $? -ne 0 -o "$KUBECONFIG" = "null" ]; then
+REQ_KUBECONFIG=$(curl -k -s -X POST -H "Authorization: Bearer $CREATORTOKEN" $RANCHER_API/clusters/local?action=generateKubeconfig)
+KUBECONFIG=$(echo $REQ_KUBECONFIG | jq -r '.config')
+if [ "$KUBECONFIG" = "null" ]; then
   echo "Could not obtain a kubeconfig"
+  echo $REQ_KUBECONFIG
   exit 1
 fi
 echo "Obtained a kubeconfig for the cluster-creator user"
