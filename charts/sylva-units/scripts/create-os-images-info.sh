5315254081fad53ec4ef0248ac8145d2f0e9770d
Cristian Manda <cristian.manda@orange.com>
2023-12-22 10:04:37 +0000

    Apply comments

--- charts/sylva-units/scripts/create-os-images-info.sh
+++ charts/sylva-units/scripts/create-os-images-info.sh
@@ -1,11 +1,20 @@
 echo "Create image details file"
-echo -e "data:\n  values.yaml: |\n    osImages:" > /tmp/os-image-details.yaml
+cat <<EOF>> /tmp/os-image-details.yaml
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: os-images-info
+  namespace: sylva-system
+data:
+  values.yaml: |
+    osImages:
+EOF
 yq '.osImages | keys | .[]' /opt/images.yaml | while read line; do
   echo "Insert image details "
-  export LINE=$line
-  echo "      $LINE:" | sed 's/[._]/-/g' >> /tmp/os-image-details.yaml
+  export OS_IMAGE_KEY=$line
+  echo "      $OS_IMAGE_KEY:" | sed 's/[._]/-/g' >> /tmp/os-image-details.yaml
   # Check if the artifact is a Sylva diskimage-builder artifact
-  uri=$(yq '.osImages.[env(LINE)].uri' /opt/images.yaml)
+  uri=$(yq '.osImages.[env(OS_IMAGE_KEY)].uri' /opt/images.yaml)
   if [[ "$uri" == *"sylva-elements/diskimage-builder"* ]]; then
     echo "This is a Sylva diskimage-builder image. Updating image details from artifact"
     url=$(echo $uri| sed 's|oci://||')
@@ -14,13 +23,8 @@ yq '.osImages | keys | .[]' /opt/images.yaml | while read line; do
     echo $manifest | yq '.annotations |with_entries(select(.key|contains("sylva")))' -P | sed "s|.*/|        |" >> /tmp/os-image-details.yaml
   fi
   echo "Adding user provided details"
-  yq '.osImages.[env(LINE)]' /opt/images.yaml | sed 's/^/        /' >> /tmp/os-image-details.yaml
+  yq '.osImages.[env(OS_IMAGE_KEY)]' /opt/images.yaml | sed 's/^/        /' >> /tmp/os-image-details.yaml
 done
-# Verify os-images-info configmap exists or create an empty one
-if ! kubectl -n sylva-system get cm os-images-info &>/dev/null; then
-  # Initialize new configmap
-  kubectl -n sylva-system create configmap os-images-info
-fi
 # Update configmap
 echo "Updating configmap"
-kubectl -n sylva-system patch configmap os-images-info --patch-file /tmp/os-image-details.yaml
+kubectl apply -f /tmp/os-image-details.yaml
