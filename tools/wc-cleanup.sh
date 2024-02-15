7b98e8e90d38219d76029897e252b8452416bcc8
Akshay Akshay <akshay.akshay@orange.com>
2024-02-15 16:38:31 +0530

    wc-cleanup

---
+++ tools/wc-cleanup.sh
@@ -0,0 +1,20 @@
+WORKLOAD_CLUSTER=$1
+if [[ -z "$2" ]]; then
+  CLUSTER="cluster"
+else
+  CLUSTER=$2
+fi
+
+if [[ -z "$1" ]]; then
+  echo "Namespace is not defined"
+else
+  echo $WORKLOAD_CLUSTER - $CLUSTER
+  source bin/env
+  export KUBECONFIG=management-cluster-kubeconfig
+  flux suspend --all ks -n $CLUSTER
+  flux suspend --all hr -n $CLUSTER
+  kubectl patch  -n $WORKLOAD_CLUSTER clusters.cluster.x-k8s.io $CLUSTER --type merge -p '{"spec":{"paused": false}}'
+  kubectl delete -n $WORKLOAD_CLUSTER hr $CLUSTER
+  kubectl delete -n $WORKLOAD_CLUSTER heatstacks heatstack-capo-cluster-resources
+  kubectl delete ns $WORKLOAD_CLUSTER
+fi
