a5319264a5e4d6b15761fbdd85e64f7b881b779b
Remi Le Trocquer <remi.letrocquer@orange.com>
2024-03-01 17:20:02 +0100

    Test

--- tools/kind/bootstrap-cluster.sh
+++ tools/kind/bootstrap-cluster.sh
@@ -80,6 +80,7 @@ fi
 # Try to retrieve bootstrap_ip config in values.yaml and expose ironic and os-image-server ports if defined
 if yq -e '.metal3.bootstrap_ip' ${VALUES_FILE} &>/dev/null; then
     BOOTSTRAP_IP=$(yq -e '.metal3.bootstrap_ip' ${VALUES_FILE})
+    BOOTSTRAP_IP="0.0.0.0"
     for port in "5050/TCP" "6180/TCP" "6185/TCP" "6385/TCP" "80/TCP" "443/TCP"; do
         KIND_CONFIG=$(echo "$KIND_CONFIG" | yq '.nodes[0].extraPortMappings += [{"containerPort": '${port%/*}', "hostPort": '${port%/*}', "listenAddress": "'$BOOTSTRAP_IP'", "protocol": "'${port#*/}'"}]')
     done
