b60a6400fd41aebc9ed9b2b3790a8e58611b1fce
Akshay Akshay <akshay.akshay@orange.com>
2024-03-26 15:15:29 +0530

    wc to workload cluster

--- tools/workload-cluster-cleanup.sh
+++ tools/workload-cluster-cleanup.sh
@@ -4,7 +4,7 @@ set -e
 export BASE_DIR="$(realpath $(dirname $0)/.. )"
 
 if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
-    echo "Usage: ./wc-cleanup.sh <WORKLOAD_CLUSTER_NAMESPACE > <(optionally)IReallyWantToDelete>"
+    echo "Usage: ./workload-cluster-cleanup.sh <WORKLOAD_CLUSTER_NAMESPACE > <(optionally)IReallyWantToDelete>"
     exit 1
 fi
 
@@ -22,7 +22,7 @@ if [[ $CONFIRMATION != "IReallyWantToDelete" ]]; then
   exit 0
 fi
 
-echo "Running wc-cleanup.sh from dir: ${BASE_DIR}"
+echo "Running workload-cluster-cleanup.sh from dir: ${BASE_DIR}"
 
 if [[ -f ${BASE_DIR}/management-cluster-kubeconfig ]]; then
     export KUBECONFIG=${BASE_DIR}/management-cluster-kubeconfig
@@ -46,8 +46,6 @@ kubectl delete --request-timeout 5m -n $WORKLOAD_CLUSTER_NAMESPACE hr cluster
 if kubectl get -n $WORKLOAD_CLUSTER_NAMESPACE heatstacks &> /dev/null; then
     echo "Found heatstacks in namespace $WORKLOAD_CLUSTER_NAMESPACE, deleting..."
     kubectl delete --request-timeout 1m -n $WORKLOAD_CLUSTER_NAMESPACE heatstacks heatstack-capo-cluster-resources
-else
-    echo "No heatstacks found in namespace $WORKLOAD_CLUSTER_NAMESPACE, skipping deletion"
 fi
 kubectl delete --request-timeout 1m ns $WORKLOAD_CLUSTER_NAMESPACE
 echo "Successfully deleted workload cluster in namespace \"$WORKLOAD_CLUSTER_NAMESPACE\""
