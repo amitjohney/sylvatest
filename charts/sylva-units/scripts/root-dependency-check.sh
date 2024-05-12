9986eccd0c59119dba072d51fad8867ea65e6cc9
Thomas Morin <thomas.morin@orange.com>
2024-05-12 19:17:46 +0000

    Apply 3 suggestion(s) to 2 file(s)
    Co-authored-by: Bogdan-Adrian Burciu <bogdanadrian.burciu@yahoo.com>

--- charts/sylva-units/scripts/root-dependency-check.sh
+++ charts/sylva-units/scripts/root-dependency-check.sh
@@ -18,7 +18,7 @@ set -o pipefail
 
 # how long to wait for all Kustomizations to meet the criteria
 # (we shouldn't need to wait for long, except that if k8s API is slow
-#  it may require sometime to kubectl to see all Kustomizations, and
+#  it may require some time for kubectl to see all Kustomizations, and
 #  unfortunately it may declare that a Kustomization didn't meet the wait
 #  criteria even if just didn't had time to see it)
 WAIT_TIMEOUT=${WAIT_TIMEOUT:-60s}
@@ -33,7 +33,7 @@ trap exit_trap EXIT
 
 # TODO: check if something special needs to be done on suspended resources ...
 
-echo "--- waiting for Kustomization to be labbeled with sylva-units-helm-revision=$HELM_REVISION"
+echo "--- waiting for Kustomization to be labeled with sylva-units-helm-revision=$HELM_REVISION"
 
 kubectl -n $TARGET_NAMESPACE wait Kustomization -l app.kubernetes.io/instance=sylva-units --timeout $WAIT_TIMEOUT \
   --for=jsonpath="{.metadata.labels.sylva-units-helm-revision}=$HELM_REVISION"
