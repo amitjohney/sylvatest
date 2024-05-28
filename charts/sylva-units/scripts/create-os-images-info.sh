0e488460a693a525e413793965494526dd6815ef
Anton Daniel <daniel.anton@orange.com>
2024-05-28 12:12:45 +0000

    Update script create os image info

--- charts/sylva-units/scripts/create-os-images-info.sh
+++ charts/sylva-units/scripts/create-os-images-info.sh
@@ -22,6 +22,8 @@ EOF
 
 echo "Looping over OS images..."
 
+MAX_IMAGE_SIZE=0
+
 yq '.os_images | keys | .[]' /opt/images.yaml | while read os_image_key; do
   echo "-- processing image $os_image_key"
   export os_image_key
@@ -36,11 +38,18 @@ yq '.os_images | keys | .[]' /opt/images.yaml | while read os_image_key; do
     insecure=$([[ $oci_registry_insecure == "true" ]] && echo "--insecure" || true)
     manifest=$(oras manifest fetch $url $insecure)
     echo $manifest | yq '.annotations |with_entries(select(.key|contains("sylva")))' -P | sed "s|.*/|        |" >> $configmap_file
+    current_image_size=$(echo $manifest | yq eval '.annotations |with_entries(select(.key|contains("size")))' -o=json | jq 'map(tonumber) | add / 1073741824 | ceil')
+    if (( current_image_size > MAX_IMAGE_SIZE )); then
+      MAX_IMAGE_SIZE=$current_image_size
+    fi
   fi
   echo "Adding user provided details"
   yq '.os_images.[env(os_image_key)] |del(.. | select(has("sylva_dib_image")).sylva_dib_image)' /opt/images.yaml | sed 's/^/        /' >> $configmap_file
   echo ---
 done
+
+echo "Adding maximum image size"
+echo "  $MAX_IMAGE_SIZE" >> $configmap_file
 
 # Update configmap
 echo "Updating ${OUTPUT_CONFIGMAP} configmap"
