c76e8b14eae830e80c7ae8b007743ea0f582bd54
Anton Daniel <daniel.anton@orange.com>
2024-05-29 07:52:41 +0000

    more debug

--- charts/sylva-units/scripts/create-os-images-info.sh
+++ charts/sylva-units/scripts/create-os-images-info.sh
@@ -23,6 +23,7 @@ EOF
 echo "Looping over OS images..."
 
 MAX_IMAGE_SIZE=0
+export MAX_IMAGE_SIZE
 
 yq '.os_images | keys | .[]' /opt/images.yaml | while read os_image_key; do
   echo "-- processing image $os_image_key"
@@ -41,15 +42,15 @@ yq '.os_images | keys | .[]' /opt/images.yaml | while read os_image_key; do
     current_image_size=$(echo $manifest | yq eval '.annotations |with_entries(select(.key|contains("size")))' -o=json | jq 'map(tonumber) | add / 1073741824 | ceil')
     if (( current_image_size > MAX_IMAGE_SIZE )); then
       MAX_IMAGE_SIZE=$current_image_size
-      export MAX_IMAGE_SIZE
-      echo "Adding maximum image size"
-      yq eval -i '.data["MAX_IMAGE_SIZE"] = strenv(MAX_IMAGE_SIZE)' "$configmap_file"
     fi
   fi
   echo "Adding user provided details"
   yq '.os_images.[env(os_image_key)] |del(.. | select(has("sylva_dib_image")).sylva_dib_image)' /opt/images.yaml | sed 's/^/        /' >> $configmap_file
   echo ---
 done
+
+echo "Adding maximum image size"
+yq eval -i '.data["MAX_IMAGE_SIZE"] = strenv(MAX_IMAGE_SIZE)' "$configmap_file"
 
 # Update configmap
 echo "Updating ${OUTPUT_CONFIGMAP} configmap"
